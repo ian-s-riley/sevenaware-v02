9eb620b7561019f84b0a09d2bda385f8413ba5f6
Ian Riley <ian.s.riley@outlook.com>
2021-02-13 15:02:47 -0700

    removed amplify

--- src/index.js
+++ src/index.js
@@ -25,10 +25,10 @@ import Admin from "layouts/Admin.js";
 
 import "assets/css/material-dashboard-react.css?v=1.9.0";
 
-//aws amplify for auth
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
+// //aws amplify for auth
+// import Amplify from 'aws-amplify';
+// import config from './aws-exports';
+// Amplify.configure(config);
 
 const hist = createBrowserHistory();
 
