32c397f304f1598528e6dd90d34b84a721a79713
Ian Riley <ian.s.riley@outlook.com>
2021-02-24 18:19:51 -0700

    whatever updates

---
+++ src/graphql/queries.js
@@ -0,0 +1,107 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getForm = /* GraphQL */ `
+  query GetForm($id: ID!) {
+    getForm(id: $id) {
+      id
+      name
+      code
+      order
+      description
+      helpCategory
+      helpTitle
+      helpDescription
+      legal
+      parentFormId
+      parentForm
+      isArray
+      isComplete
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listForms = /* GraphQL */ `
+  query ListForms(
+    $filter: ModelFormFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listForms(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        code
+        order
+        description
+        helpCategory
+        helpTitle
+        helpDescription
+        legal
+        parentFormId
+        parentForm
+        isArray
+        isComplete
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getField = /* GraphQL */ `
+  query GetField($id: ID!) {
+    getField(id: $id) {
+      id
+      name
+      code
+      description
+      fieldType
+      order
+      value
+      defaultValue
+      options
+      userId
+      lenderId
+      label
+      helpText
+      image
+      formId
+      form
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listFields = /* GraphQL */ `
+  query ListFields(
+    $filter: ModelFieldFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listFields(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        code
+        description
+        fieldType
+        order
+        value
+        defaultValue
+        options
+        userId
+        lenderId
+        label
+        helpText
+        image
+        formId
+        form
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,138 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateForm = /* GraphQL */ `
+  subscription OnCreateForm {
+    onCreateForm {
+      id
+      name
+      code
+      order
+      description
+      helpCategory
+      helpTitle
+      helpDescription
+      legal
+      parentFormId
+      parentForm
+      isArray
+      isComplete
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateForm = /* GraphQL */ `
+  subscription OnUpdateForm {
+    onUpdateForm {
+      id
+      name
+      code
+      order
+      description
+      helpCategory
+      helpTitle
+      helpDescription
+      legal
+      parentFormId
+      parentForm
+      isArray
+      isComplete
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteForm = /* GraphQL */ `
+  subscription OnDeleteForm {
+    onDeleteForm {
+      id
+      name
+      code
+      order
+      description
+      helpCategory
+      helpTitle
+      helpDescription
+      legal
+      parentFormId
+      parentForm
+      isArray
+      isComplete
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateField = /* GraphQL */ `
+  subscription OnCreateField {
+    onCreateField {
+      id
+      name
+      code
+      description
+      fieldType
+      order
+      value
+      defaultValue
+      options
+      userId
+      lenderId
+      label
+      helpText
+      image
+      formId
+      form
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateField = /* GraphQL */ `
+  subscription OnUpdateField {
+    onUpdateField {
+      id
+      name
+      code
+      description
+      fieldType
+      order
+      value
+      defaultValue
+      options
+      userId
+      lenderId
+      label
+      helpText
+      image
+      formId
+      form
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteField = /* GraphQL */ `
+  subscription OnDeleteField {
+    onDeleteField {
+      id
+      name
+      code
+      description
+      fieldType
+      order
+      value
+      defaultValue
+      options
+      userId
+      lenderId
+      label
+      helpText
+      image
+      formId
+      form
+      createdAt
+      updatedAt
+    }
+  }
+`;
